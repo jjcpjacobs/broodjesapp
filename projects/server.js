b98b76bc3edc227b844e035790da235aee950c95
Hans Jacobs <jjcp.jacobs@gmail.com>
2018-04-14 15:37:20 +0200

    Poging 2

--- projects/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello World!!!');
-    console.log('get');
-});
-
-app.listen(8080, () => console.log('Example app listening on port 8080!'));

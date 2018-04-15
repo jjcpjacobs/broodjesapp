a5d88313f60c52ec61a28fbd13ffd0ae6f7af280
Hans Jacobs <jjcp.jacobs@gmail.com>
2018-04-15 14:27:45 +0200

    First steps with mongoose

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-console.log('Hallo!!!');
+mongoose.connect('mongodb://mongodb/bread');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+    console.log('Connected to database');
+});
+
+const productSchema = mongoose.Schema({
+    name: String,
+    price: Number
+});
+
+const productCollection = mongoose.model('products', productSchema);
+
+let product = new productCollection({ name: 'Product 1', price: 1000 });
+
+product.save(function (err, product) {
+    if (err) return console.error(err);
+    console.log('First record saved');
+})
+
 app.get('/', (req, res) => { 
     res.send('Hello World!!!...');
     console.log('get');
